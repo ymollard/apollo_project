18206ca26be01dad74eeb171e65e5a7b11af0b49
Yoan Mollard <yoan.mollard@inria.fr>
2015-10-27 16:20:40 +0100

    Added integer reading

--- src/apollo15.cc
+++ src/apollo15.cc
@@ -1,7 +1,7 @@
 /*
  * apollo15.cc
  *
- *  Created on: Jun 29, 2012
+ *  Created on: Oct 26, 2015
  *      Author: ymollard
  */
 
@@ -18,30 +18,46 @@
 #include <param/src/param.h>
 #include <utils/src/STime.h>
 #include "DalRegister.h"
-//#include "Celestial.h"
 
 /* other includes */
 #include "apollo15.hpp"
 #include "jpleph.h"
 
-//#include "Celestial.cc" // TODO dirty thing
-
 using namespace Info;
 using namespace std;
 
 #define TRIM 			0 /* Bytes to remove to reach the first record in the file */
-#define WORD_LENGTH     6 /* Length in Bytes of a single word, int or float */
-#define INSTRUMENT_ROLL	37.75*DEG_TO_RAD  /* instrument angle around the roll axis (x) */
-#define JPLEPH_PATH 	"/home/ymollard/ctadev/lib/JPLEPH"
+#define ENCODED_WORD_LENGTH     6 /* Length in Bytes of a word as encoded in the file, int or float */
 #define OUTPUT_PATH "./fits/"
 
 #define STR_VALUE(arg) #arg
 #define CHECK_END() if(feof(f)) { cout << "File ended" << endl; f.close(); dataSetServer->close(set); exit(EXIT_SUCCESS); }
 #define ASSERT(x) if(!(x)) { cout << "At 0x" << hex << f.tellg() << dec << " assert failed: " << STR_VALUE(x) << endl; f.close(); exit(EXIT_SUCCESS); } // Allows to close all files properly when a new spectra is not complete (eof)
 
+unsigned long int read_int(ifstream &f) {
+    // Read a LSB-first int of 6 Bytes and return the corresponding encoding for the host
+    unsigned long int value = 0;
+    f.read((char *)&value, ENCODED_WORD_LENGTH);
+    value = be64toh(value);
+    // Now shift the MSB-first value to restore the 36bit int
+    value >>= 8*(sizeof(unsigned long int)-ENCODED_WORD_LENGTH);
+    return value;
+}
+
+double read_float(ifstream &f) {
+    // Read a LSB-first float of 6 Bytes and return the IEEE-754 representation
+    unsigned long int value = 0;
+    f.read((char *)&value, ENCODED_WORD_LENGTH);
+    value = be64toh(value);
+    // Now shift the MSB-first value to restore the 36bit int
+    //value >>= 8*(sizeof(unsigned long int)-ENCODED_WORD_LENGTH);
+    return 0;
+}
+
 void read_binary(ifstream &f)
 {
-    unsigned long int value;  // will store values of size WORD_LENGTH
+    unsigned long int int_val;
+    double float_val;
 
     f.ignore(TRIM);
     while(!f.eof()) {
@@ -51,53 +67,52 @@ void read_binary(ifstream &f)
         cout << "At 0x" << hex << f.tellg() << ":" << endl;
 
         for(int i=0; i<32 && !f.eof(); ++i) {
-            f.read((char *)&value, WORD_LENGTH);
-            //value = be64toh(value);
-            //trvalue = read_apollo(be64toh(value));
-
-            cout << "Trajectory parameter" << i+1 << ": " << value << endl;
+            float_val = read_float(f);
+            if(i==0) {
+                cout << "Record 1, GMT: " << float_val << endl;
+            }
+            cout << "Trajectory parameter " << dec << i+1 << ": " << float_val << endl;
         }
 
         //**** RECORD 2: 13 words, x-ray and gamma-ray common data, all ints except words 2 3 and 13 (floats)
         f.ignore(2); // remove 0x4E = 78 before record 2
 
         for(int i=0; i<13 && !f.eof(); ++i) {
-            f.read((char *)&value, WORD_LENGTH);
-            value = be64toh(value);
-
-            if(i==0) {
-                cout << "Record 2, GMT: " << dec << value << hex << endl;
+            if(i==1 || i==2 || i==12) {  // These are floats
+                float_val = read_float(f);
+            }
+            else {  // The rest is ints
+                int_val = read_int(f);
             }
 
-            //trvalue = read_apollo(be64toh(value));
+            if(i==0) {
+                cout << "Record 2, GMT: " << int_val << endl;
+            }
         }
 
         //**** RECORD 3: 13 words, housekeeping data, merged ints/floats
         f.ignore(2); // remove 0x4E = 78 before record 3
 
         for(int i=0; i<13 && !f.eof(); ++i) {
-            f.read((char *)&value, WORD_LENGTH);
-            value = be64toh(value);
+            if(i==1 || i==2 || i==3 || i==7 || i==8 || i==9) {  // These are floats
+                float_val = read_float(f);
+            }
+            else {  // The rest is ints
+                int_val = read_int(f);
+                cout << "Record 3 parameter " << dec << i+1 << ": " << int_val << endl;
+            }
 
-            if(i==0) {
-                cout << "Record 3, GMT: " << dec << value << hex << endl;
-           }
-
-            //trvalue = read_apollo(be64toh(value));
         }
 
         //**** RECORD 4: 513 words, GMT followed by gamma-ray counts, all integers
         f.ignore(2); // remove 0xC06 = 3078 before record 4
 
-        f.read((char *)&value, WORD_LENGTH);  // GMT
+        int_val = read_int(f);
+        cout << "Record 4, GMT: " << dec << int_val << endl;
+        //exit(0);
 
         for(int i=0; i<512 && !f.eof(); ++i) {
-            f.read((char *)&value, WORD_LENGTH);
-            value = be64toh(value);
-
-            if(i==0) {
-                cout << "Record 4, GMT: " << dec << value << hex << endl;
-            }
+            int_val = read_int(f);
 
             //if(value>0) cout << i << ": " << value << endl;
             //trvalue = read_apollo(be64toh(value));
@@ -106,7 +121,7 @@ void read_binary(ifstream &f)
 }
 
 int main(int argc, char **argv) {
-    assert(sizeof(unsigned long int)>WORD_LENGTH); // Only 64 bit machines may handle enough space in only one value
+    assert(sizeof(unsigned long int)>ENCODED_WORD_LENGTH); // Only 64 bit machines may handle enough space in only one value
     assert(sizeof(float)==4);
 
     errHandler.name(argv[0]);
